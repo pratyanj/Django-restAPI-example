af53b224bed82f863e19e645ff13bee28819ca54
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-10 10:42:18 +0530

    install and steup

---
+++ core/base/views.py
@@ -0,0 +1,29 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET'])
+def index(request):
+    courses = {
+        'courses': [
+            {
+                'id': 1,
+                'title': 'Python',
+                'description': 'Python is a programming language',
+                'price': 100
+            },
+            {
+                'id': 2,
+                'title': 'JavaScript',
+                'description': 'JavaScript is a programming language',
+                'price': 200
+            },
+            {
+                'id': 3,
+                'title': 'Django',
+                'description': 'Django is a web framework',
+                'price': 300
+            }
+        ]
+    }
+    return Response(courses)
