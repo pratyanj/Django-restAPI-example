9b5ef93240acb11c259d12f79561bcef88c3c886
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-24 17:11:12 +0530

    add gender table

--- core/base/models.py
+++ core/base/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.db.models import EmailField
 
-
+class gender(models.Model):
+    sex = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.sex
 # Create your models here.
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     address = EmailField()
+    sex = models.ForeignKey(gender, on_delete=models.CASCADE,related_name='gender')
