f71cefffc82d6fdcaf4e7f0fbfb3961c13e27603
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-24 12:18:20 +0530

    create put , patch and delete method

--- core/base/views.py
+++ core/base/views.py
@@ -38,20 +38,48 @@ def index(request:requests.Request):
         print("DELETE method called")
     return Response(courses)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'PATCH','DELETE'])
 def person_data(request:requests.Request):
     if request.method == "GET":
         print("GET method called")
         person = Person.objects.all()
         serializer_person = PersonSerializer(person, many=True)
         return Response(serializer_person.data)
-    elif request.method == "POST":
+    
+    elif request.method == "POST":  
         print("POST method called")
         serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+    elif request.method == "PUT":
+        print("PUT method called")
+        print(request.data)
+        person = Person.objects.get(id = request.data['id'])
+        serializer = PersonSerializer(person, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)   
+     
+    elif request.method == "PATCH":
+        print("PATCH method called")
+        person = Person.objects.get(id = request.data['id'])
+        serializer = PersonSerializer(person, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == "DELETE":
+        print("DELETE method called")
+        person = Person.objects.get(id = request.data['id'])
+        person.delete()
+        return Response("Person deleted successfully")
+        
+    
 json = {
 "name":"pratyanj",
 "age":2,
