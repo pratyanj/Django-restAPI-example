336f6551782c9b6eccc01ddb618962e990867b3f
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-24 11:38:53 +0530

    migrate database

--- core/base/models.py
+++ core/base/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models import EmailField
+
 
 # Create your models here.
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    address = models.CharField(max_length=100)
+    address = EmailField()
