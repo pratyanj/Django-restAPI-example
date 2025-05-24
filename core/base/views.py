b9c4b89caf2ff03e09765c60158fb369ccc1e207
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-24 11:40:27 +0530

    create person API

--- core/base/views.py
+++ core/base/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import PersonSerializer
+from .models import Person
+import requests
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 # @api_view(['GET'])
-def index(request):
+def index(request:requests.Request):
     courses = {
         'courses': [
             {
@@ -35,3 +37,23 @@ def index(request):
     elif request.method == "DELETE":
         print("DELETE method called")
     return Response(courses)
+
+@api_view(['GET', 'POST'])
+def person_data(request:requests.Request):
+    if request.method == "GET":
+        print("GET method called")
+        person = Person.objects.all()
+        serializer_person = PersonSerializer(person, many=True)
+        return Response(serializer_person.data)
+    elif request.method == "POST":
+        print("POST method called")
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+json = {
+"name":"pratyanj",
+"age":2,
+"address":"pratyanj@gmail.com"
+}
