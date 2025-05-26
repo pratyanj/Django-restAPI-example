4bdde3f0c6c1b8cb5376f99883aaf23832c3f792
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-26 12:04:38 +0530

    Display data on admin page

--- core/base/admin.py
+++ core/base/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Person, gender
+from .models import Person, Gender
 # Register your models here.
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address')
+    list_display = ('name', 'address',"sex",'age')
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(gender)
+admin.site.register(Gender)
