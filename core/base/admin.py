f428932da855e420d2e6df2d0e6bc32f020ba44a
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-05-26 11:25:32 +0530

    admin page update

--- core/base/admin.py
+++ core/base/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Person, gender
 # Register your models here.
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(gender)
