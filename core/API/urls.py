7503e713bb6f6eea49afb6a58714e85dfd1187fb
Pratyanj <pratyanjmodh1205@gmail.com>
2025-06-21 10:42:42 +0530

    small chnage
    small change url path

--- core/API/urls.py
+++ core/API/urls.py
@@ -6,7 +6,7 @@ from base.views import PersonViewSet
 router = DefaultRouter()
 router.register('person_class', PersonViewSet, basename='person')
 urlpatterns = [
-     path('', include(router.urls)),
+    path('', include(router.urls)),
     path('index/', index ),
     path('person/', person_data),
     path('login/', login ),
